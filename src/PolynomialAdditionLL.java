1ed2243bec7524c13232e0cfc8db96ea5054a74a
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-23 23:00:47 -0700

    Initial commit

---
+++ src/PolynomialAdditionLL.java
@@ -0,0 +1,17 @@
+public class PolynomialAdditionLL {
+    static class Node{
+        int data1, data2;
+        Node next = null;
+        Node(int data1, int data2){
+            this.data1 = data1;
+            this.data2 = data2;
+            this.next = null;
+            
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
