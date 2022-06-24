642d6a54e910702da6265afb54409eaad07a9243
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-23 23:19:56 -0700

    added the comments for future references

--- src/PolynomialAdditionLL.java
+++ src/PolynomialAdditionLL.java
@@ -10,30 +10,38 @@ public class PolynomialAdditionLL {
         }
     }
         public static Node polynomialAdditionLL(Node p1, Node p2){
-
+        //if null then return the other one
         if(p1 == null){
             return p2;
         }
         if(p2 == null){
             return p1;
         }
+        // nodes , one to travel thru
         Node head = new Node(0,0);
         Node temp = head;
-        while( p1 != null && p2 !=null){
-            if(p1.pow == p2.pow){
 
+        //until they become null
+        while( p1 != null && p2 !=null){
+
+            //if they have same power
+            if(p1.pow == p2.pow){
+                // then add their coefficients
                 p1.coeff = p1.coeff + p2.coeff;
+                //and update the pointers and move forward
                 temp.next = p1;
                 temp = temp.next;
                 p1 = p1.next;
                 p2 = p2.next;
 
             }
+            //if p1.pow is greater then we move to the smaller power of p1
             else if (p1.pow> p2.pow){
                 temp.next = p1;
                 temp = temp.next;
                 p1 = p1.next;
             }
+            //if p2 has greater power then we move to the next smaller power
             else {
                 temp.next = p2;
                 temp = temp.next;
@@ -41,12 +49,15 @@ public class PolynomialAdditionLL {
 
             }
         }
+        //after the loop if there is any remaining element in any of the list
+            //then just update the new list with those elements
         if(p1 != null){
             temp.next =p1;
         }
         if(p2 !=null){
             temp.next = p2;
 
+            //and return head.next
         }return head.next;
 
         }
