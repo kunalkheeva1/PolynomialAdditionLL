f6944cdf42808391ecdf7440e79b4af1fa6b0488
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-23 23:14:50 -0700

    created a method of adding two polynomials nodes

--- src/PolynomialAdditionLL.java
+++ src/PolynomialAdditionLL.java
@@ -1,15 +1,55 @@
 public class PolynomialAdditionLL {
     static class Node{
-        int data1, data2;
+        int coeff, pow;
         Node next = null;
-        Node(int data1, int data2){
-            this.data1 = data1;
-            this.data2 = data2;
+        Node(int coeff, int pow){
+            this.coeff = coeff;
+            this.pow = pow;
             this.next = null;
-            
+
         }
     }
+        public static Node polynomialAdditionLL(Node p1, Node p2){
 
+        if(p1 == null){
+            return p2;
+        }
+        if(p2 == null){
+            return p1;
+        }
+        Node head = new Node(0,0);
+        Node temp = head;
+        while( p1 != null && p2 !=null){
+            if(p1.pow == p2.pow){
+
+                p1.coeff = p1.coeff + p2.coeff;
+                temp.next = p1;
+                temp = temp.next;
+                p1 = p1.next;
+                p2 = p2.next;
+
+            }
+            else if (p1.pow> p2.pow){
+                temp.next = p1;
+                temp = temp.next;
+                p1 = p1.next;
+            }
+            else {
+                temp.next = p2;
+                temp = temp.next;
+                p2 = p2.next;
+
+            }
+        }
+        if(p1 != null){
+            temp.next =p1;
+        }
+        if(p2 !=null){
+            temp.next = p2;
+
+        }return head.next;
+
+        }
 
     public static void main(String[] args) {
 
